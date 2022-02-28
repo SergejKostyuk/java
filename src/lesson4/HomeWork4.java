b809004f63295b3baa310f3e64c9675b557ba85f
Sergej <serega95kost@gmail.com>
2022-02-28 17:11:43 +0300

    HomeWork4

---
+++ src/lesson4/HomeWork4.java
@@ -0,0 +1,31 @@
+package lesson4;
+import java.util.*;
+
+public class HomeWork4 {
+    public static void main(String[] args) {
+        String[] words = {"dog", "cat", "pet", "lock", "mock", "lock", "fork", "mock", "golf", "lock"};
+
+        // Task 1.a
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        System.out.println(map);
+        System.out.println("");
+
+        // subtask 1.b.
+        Set<String> uniq = new HashSet<>(Arrays.asList(words));
+        System.out.println(uniq);
+        System.out.println("");
+
+        // subtask 2
+        Phonebook pb = new Phonebook();
+
+        pb.add("Ivanov", "123");
+        pb.add("Petrov", "456");
+        pb.add("Petrov", "789");
+
+        System.out.println(pb.get("Petrov"));
+    }
+
+}
