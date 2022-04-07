48941515617c1e12df0bd2f7ca70c217ccfea552
Sergej <serega95kost@gmail.com>
2022-04-07 23:41:26 +0300

    homeWork5

--- src/lesson5/HomeWork5.java
+++ src/lesson5/HomeWork5.java
@@ -1,18 +1,14 @@
 package lesson5;
+
 import java.io.FileWriter;
+import java.io.FileReader;
+import org.json.*;
 import java.io.IOException;
 
 public class HomeWork5 {
-    public static void main(String [] args)
-    {
-        generateCsvFile("d:\\test.csv");
-    }
-
-    private static void generateCsvFile(String sFileName)
-    {
-        try
-        {
-            FileWriter writer = new FileWriter(sFileName);
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("test.csv");
 
             writer.append("Name");
             writer.append(',');
@@ -29,14 +25,19 @@ public class HomeWork5 {
             writer.append("demo@demo.com");
             writer.append('\n');
 
-            //generate whatever data you want
-
             writer.flush();
             writer.close();
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        try (FileReader reader = new FileReader("test.txt")) {
+            String str;
+            while ((str = reader.readLine()) != null) {
+                System.out.println(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
